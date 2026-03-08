9f59d5a9ae8d7fc99d6ca145e049de2e3a4645bb
KumananMahalingam <mkumanan@outlook.com>
2026-03-08 14:11:13 -0400

    basic forget password logic

--- auth.py
+++ auth.py
@@ -45,3 +45,33 @@ def register(email: str, password: str, first_name: str, last_name: str) -> dict
         raise Exception(f"Login failed: [{response['ErrorCode']}] {response['Description']}")
 
     return response
+
+def get_profile(access_token: str) -> dict:
+    """
+    Get the profile of a user with the access token.
+    Returns the response dict containing the profile on success.
+    """
+    response = lr.authentication.get_profile_by_access_token(
+        access_token = access_token
+    )
+
+    if "ErrorCode" in response:
+        raise Exception(f"Get profile failed: [{response['ErrorCode']}] {response['Description']}")
+
+    return response
+
+def forgot_password(email: str, reset_url: str = "https://yourapp.com/reset") -> dict:
+    """
+    Send a password reset email to user.
+    """
+
+    response = lr.authentication.forgot_password(
+        email = email,
+        reset_password_url = reset_url,
+        email_template = None
+    )
+
+    if "ErrorCode" in response:
+        raise Exception(f"Forgot password failed: {response['Description']}")
+
+    return response
